6326ff052bd2977cb4b7b1d910e2975b03a782a9
Thomas Eubank <eubanktr@gmail.com>
2021-08-18 09:41:45 -0400

    updated server file

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
